3774b54fb9916c05a97c6166d24fbc027c57b9b4
Rate Shopper System <sistema@rateShopper.com>
2025-07-09 15:31:29 -0400

    Atualizacao credenciais banco OSH-IA

--- backend/database.js
+++ backend/database.js
@@ -3,28 +3,28 @@ const mysql = require('mysql2/promise');
 // Configuração do banco de dados com fallbacks
 const dbConfigs = [
   {
-    name: 'Produção OSH',
-    host: 'osh-apps_mariaddb-rateshopper',
+    name: 'Produção OSH-IA',
+    host: 'osh-ia_mariadb-ia',
     port: 3306,
-    user: 'rateshopper',
-    password: 'OSH4040()Xx!..nn',
-    database: 'rateshopper'
+    user: 'mariadb',
+    password: 'OSH4040()Xx!..n',
+    database: 'osh-ia'
+  },
+  {
+    name: 'Host Externo',
+    host: '147.93.36.138',
+    port: 3306,
+    user: 'mariadb',
+    password: 'OSH4040()Xx!..n',
+    database: 'osh-ia'
   },
   {
     name: 'Localhost',
     host: 'localhost',
     port: 3306,
-    user: 'rateshopper',
-    password: 'OSH4040()Xx!..nn',
-    database: 'rateshopper'
-  },
-  {
-    name: '127.0.0.1',
-    host: '127.0.0.1',
-    port: 3306,
-    user: 'rateshopper',
-    password: 'OSH4040()Xx!..nn',
-    database: 'rateshopper'
+    user: 'mariadb',
+    password: 'OSH4040()Xx!..n',
+    database: 'osh-ia'
   }
 ];
 
